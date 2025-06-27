9f84715a007d3e900ff11be0dd325bdb3f45807d
martinww9 <martin.vasquez.o@mail.pucv.cl>
2025-06-27 00:27:07 -0400

    Eliminados archivos extra

--- controllers/my_controller/build/release/my_controller.d
+++
@@ -1,6 +0,0 @@
-build/release/my_controller.o: my_controller.c \
- C:\Users\ACER\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
- C:\Users\ACER\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
- C:\Users\ACER\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
- C:\Users\ACER\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
- C:\Users\ACER\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h
